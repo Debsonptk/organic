54ab51ddd2270d3a01402544242470e739a2c007
DebsonCoelho <debsoncoelh@gmail.com>
2023-09-06 10:09:18 -0300

    fix footer

--- src/components/BarFooter/index.jsx
+++ src/components/BarFooter/index.jsx
@@ -9,14 +9,17 @@ const BarFooter = () => (
         <div className="container">
             <div className="row justify-content-between">
                 <div className="col d-flex align-items-center">
-                    <p>Copyright © 2023 Designed by AuThemes. All rights reserved.</p>
+                    <p className="d-none d-sm-block">Copyright © 2023 Designed by AuThemes. All rights reserved.</p>
                 </div>
                 <div className="col d-flex justify-content-end pt-4 pb-4">
-                    <img src={discover} alt="discover" className="px-2"/>
-                    <img src={paypal} alt="paypal" className="px-2"/>
-                    <img src={visa} alt="visa" className="px-2"/>
-                    <img src={master} alt="master" className="px-2"/>
+                    <img src={discover} alt="discover" className="px-2 img-fluid"/>
+                    <img src={paypal} alt="paypal" className="px-2 img-fluid"/>
+                    <img src={visa} alt="visa" className="px-2 img-fluid"/>
+                    <img src={master} alt="master" className="px-2 img-fluid"/>
                 </div>
+            </div>
+            <div className="col d-flex align-items-center">
+                <p className="d-sm-none text-center">Copyright © 2023 Designed by AuThemes. All rights reserved.</p>
             </div>
         </div>
     </CoverBarFooter>
